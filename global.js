d5e447da01a4f58c24b27c9c87359e1aeae0b843
d25molina <d2molina@ucsd.edu>
2025-10-15 19:26:08 -0700

    more changes

--- global.js
+++ global.js
@@ -2,6 +2,12 @@ console.log('IT’S ALIVE!');
 
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));}
+
+const BASE_PATH =
+  location.hostname === 'localhost' || location.hostname === '127.0.0.1'
+    ? '/'
+    : '/portfolio/';
+
 let pages = [
   { url: '', title: 'Home' },
   { url: 'projects/', title: 'My Projects' },
@@ -9,8 +15,7 @@ let pages = [
   { url: 'resume/', title: 'My Resume' },
   { url: 'https://github.com/d25molina', title: 'My GitHub Profile' }
 ];
-let nav = document.createElement('nav');
-document.body.prepend(nav);
+
 document.body.insertAdjacentHTML(
   'afterbegin',
   `
@@ -34,6 +39,10 @@ select.addEventListener('input', function (event) {
   localStorage.colorScheme = event.target.value
 });
 
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+
 for (let p of pages) {
   let url = p.url;
   let title = p.title;
@@ -41,17 +50,13 @@ for (let p of pages) {
   a.href = url;
   a.textContent = title;
   nav.append(a);
-const BASE_PATH =
-  location.hostname === 'localhost' || location.hostname === '127.0.0.1'
-    ? '/'
-    : '/portfolio/';
-url = !url.startsWith('http') ? BASE_PATH + url : url;
-if (a.host === location.host && a.pathname === location.pathname.endsWith(a.pathname)) {
-  a.classList.add('current');
+  url = !url.startsWith('http') ? BASE_PATH + url : url;
+  if (a.host === location.host && a.pathname === location.pathname) {
+    a.classList.add('current');
 }
-const navLinks = document.querySelectorAll("nav a");
-navLinks.forEach(a => {
-  if (a.host && a.host !== location.host) {
-    a.target = "_blank";
-  }
+    const navLinks = document.querySelectorAll("nav a");
+    navLinks.forEach(a => {
+        if (a.host && a.host !== location.host) {
+            a.target = "_blank";
+}
 })};
